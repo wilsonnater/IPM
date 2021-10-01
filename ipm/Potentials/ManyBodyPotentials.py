fixed Force issue with negative sign, structure container dr had wrong sign. Added in sutton chen EAM with cutoff to manybodypotetnials

ipm/Potentials/ManyBodyPotentials.py
@@ -3,6 +3,8 @@ import numpy as np
 class GeneralizedSuttonChenEAM:
     def __init__(self,StructureContainer=[],A=None,B=None,C=None):
         """
+        
+        Equation is E_i = (sum 1/(r_ij^B))^C + sum 1/(r_ij^A)
         Attributes
         -----------
         hyperparameters : dictionary(hyperparamters)
@@ -78,11 +80,10 @@ class GeneralizedSuttonChenEAM:
                 tempsitedensity.append(sitedenistyterm[nieghs])
 
             #-0.5 is sutton chen value after derivative for the potential from poet
-            tempsitedensity=((np.array(tempsitedensity)**(-self._C))+((sitedenistyterm[site['Site']])**(-self._C))).reshape(-1,1)
-            print(tempsitedensity)
+            tempsitedensity=((np.array(tempsitedensity)**((self._C-1)))+((sitedenistyterm[site['Site']])**((self._C-1)))).reshape(-1,1)
             #-9 and -6 is sutton chen value for the potential from POET
-            tempdfeat = np.array([((-self._A)*site['dradius']*((site['radius'].reshape(-1,1))**((-self._A)-1))),
-                           ((self._C)*tempsitedensity*(self._B)*site['dradius']*((site['radius'].reshape(-1,1))**((-self._B)-1)))])
+            tempdfeat = (-1)*np.array([((-self._A)*site['dradius']*((site['radius'].reshape(-1,1))**((-self._A)-1))),
+                           ((self._C)*tempsitedensity*(-self._B)*site['dradius']*((site['radius'].reshape(-1,1))**((-self._B)-1)))])
 
             #tempdfeat.sum(1) for right feature style
             dfeature.append(tempdfeat.T)
@@ -98,3 +99,103 @@ class SuttonChenEAM(GeneralizedSuttonChenEAM):
         super().__init__(StructureContainer,9,6,0.5)
         
         
+class GeneralizedSuttonChenEAMWithCutoff:
+    def __init__(self,cutoff,StructureContainer=[],A=None,B=None,C=None):
+        """
+        Attributes
+        -----------
+        hyperparameters : dictionary(hyperparamters)
+            dicitonary with relevant hyperparameters for model
+        StructureContainer : StructureContainer2Body or StructureContainer3Body(Not yet implemented)
+            Structure Container with structures to be featurized
+        """
+        
+        self._cutoff=cutoff
+        Anynone=False
+        
+        if A ==None:
+            self._A = 9
+            print("Using A=9 from Sutton Chen Cite")
+            Anynone=True
+        else:
+            self._A = A
+        
+        if B ==None:
+            self._B = 6
+            print("Using B=6 from Sutton Chen Cite")
+            Anynone=True
+        else:
+            self._B = B
+            
+        if C ==None:
+            self._C = 0.5
+            print("Using C=0.5 from Sutton Chen Cite")
+            Anynone=True
+        else:
+            self._C = C
+            
+        if Anynone:
+            print("CITATION")
+            print("See NEED TO PUT IN SOURCE FOR EQUATION for equation details")
+        
+            
+        self._StructureContainer=StructureContainer
+            
+        self._all_features=[]
+        self._all_dfeatures=[]
+        self._all_sfeatures=[]
+        
+    def StructureProcesseror(self,StructureContainer=None):
+        if StructureContainer==None:
+            StructureContainer = self._StructureContainer
+
+
+        for count,structure in enumerate(StructureContainer):
+            energy,forces,stress=self._feature_generator(structure,count)
+            self._all_features.append(energy)
+            self._all_dfeatures.append(forces)
+            self._all_sfeatures.append(stress)
+
+    def _feature_generator(self,Structure_NeighborInfo,StructNumber):
+        feature=[]
+        dfeature=[]
+        sfeatures=[]
+
+        sitedenistyterm=[]
+
+        for sitenumber,site in enumerate(Structure_NeighborInfo):
+            #-6 is sutton chen value for the potential from POET
+            sitedenistyterm.append((((self._cutoff._all_features[StructNumber][sitenumber]).flatten())*(site['radius']**(-self._B))).sum())
+
+        for sitenumber,site in enumerate(Structure_NeighborInfo):
+            
+            cutoff_feats=self._cutoff._all_features[StructNumber][sitenumber]
+            cutoff_dfeats=self._cutoff._all_dfeatures[StructNumber][sitenumber]
+            
+            
+            #-9 and 0.5 is sutton chen value for the potential from POET
+            feature.append(np.array([(cutoff_feats*((site['radius'].reshape(-1,1))**(-self._A))).sum(),
+                                     -(sitedenistyterm[site['Site']]**(self._C))]).T)
+
+
+            #Gets the site density for all neighboring atoms
+            tempsitedensity=[]
+            for nieghs in site['neighborSiteNumber']:
+                tempsitedensity.append(sitedenistyterm[nieghs])
+
+            #-0.5 is sutton chen value after derivative for the potential from poet
+            tempsitedensity=((np.array(tempsitedensity)**((self._C-1)))+((sitedenistyterm[site['Site']])**((self._C-1)))).reshape(-1,1)
+            #-9 and -6 is sutton chen value for the potential from POET
+            tempdfeat = (-1)*np.array([(((-self._A)*site['dradius']*(cutoff_feats)*((site['radius'].reshape(-1,1))**((-self._A)-1)))+
+                                  ((cutoff_dfeats)*((site['radius'].reshape(-1,1))**((-self._A))))),
+                                  ((self._C)*tempsitedensity*
+                                   (((-self._B)*site['dradius']*(cutoff_feats)*((site['radius'].reshape(-1,1))**((-self._B)-1)))+
+                                   ((cutoff_dfeats)*((site['radius'].reshape(-1,1))**((-self._B))))))])
+
+            #tempdfeat.sum(1) for right feature style
+            dfeature.append(tempdfeat.T)
+            #.sum(1) for six stress features, xx,yy,zz,xy,yz,xz order
+            sfeatures.append(np.vstack([(tempdfeat*-site['relativepos']).T,
+                                        (tempdfeat*-np.roll(site['relativepos'], 1,1)).T]))
+
+        return feature,dfeature,sfeatures

ipm/Potentials/TwoBodyPotentials.py
@@ -53,7 +53,7 @@ class LJpotetnial:
                 if Return_Stress:
                     tempStressfeat+=self._all_sfeatures[structure_number][site].sum(1)
                     
-            EnergyFeatures.append(np.array(tempENEfeat))
+            EnergyFeatures.append(0.5*np.array(tempENEfeat))
             ForceFeatures.append(np.array(tempForcefeat))
             StressFeatures.append(np.array(tempStressfeat))
 
@@ -68,10 +68,6 @@ class LJpotetnial:
 
 
         return AllFeatures
-
-
-        
-
 
         
     def StructureProcesseror(self,StructureContainer=None):
@@ -92,7 +88,7 @@ class LJpotetnial:
         for site in Structure_NeighborInfo:
             feature.append(np.array([site['radius']**(-12),-site['radius']**(-6)]).T)
 
-            tempdfeat = np.array([-12*site['dradius']*((site['radius'].reshape(-1,1))**(-13)),
+            tempdfeat = (-1)*np.array([-12*site['dradius']*((site['radius'].reshape(-1,1))**(-13)),
                            6*site['dradius']*((site['radius'].reshape(-1,1))**(-7))])
 
             #tempdfeat.sum(1).T for right feature style
@@ -101,4 +97,4 @@ class LJpotetnial:
             sfeatures.append(np.vstack([(tempdfeat*-site['relativepos']).T,
                                         (tempdfeat*-np.roll(site['relativepos'], 1,1)).T]))
             
-            return feature,dfeature,sfeatures
+        return feature,dfeature,sfeatures

ipm/StructureContainer.py
@@ -113,12 +113,13 @@ class Structure2BodyContainer:
             drlist=[]
             indexlist=[]
             atomtypelist=[]
+            #dr/dx,dr/dy, and dr/dz are with respect to moving the site(not i)
             for i in neigh:
                 relativepos=i.coords-site_cords
                 r = np.linalg.norm(relativepos)
-                drdx = relativepos[0]/r
-                drdy = relativepos[1]/r
-                drdz = relativepos[2]/r
+                drdx = -relativepos[0]/r
+                drdy = -relativepos[1]/r
+                drdz = -relativepos[2]/r
 
                 relativeposlist.append(relativepos)
                 rlist.append(r)